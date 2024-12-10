15339540d12dc2850c43a36576dbc9707764530a
matsuryo0619 <ryo.matsu0619@gmail.com>
2024-12-10 16:46:00 +0900

    Update Search.js

--- Search.js
+++ Search.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded',pageLoad)
 
+function pageLoad(){
+	var textbox = document.getElementById('textbox');
+	textbox.addEventListener('keydown', enterKeyPress);
+}
+
 function enterKeyPress(event){
 	if(event.key === 'Enter'){
 		console.log('Press your Enter key.')
