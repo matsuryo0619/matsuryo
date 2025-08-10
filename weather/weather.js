1ab74cbef7603e99628a4b6285f8d495731c8413
MatsuR <ryo.matsu0619@gmail.com>
2025-08-10 12:08:52 +0900

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,1 +1,23 @@
+const Cities = document.getElementById('Cities');
 
+Cities.addEventListener('change', () => {
+  console.log(Cities.value);
+});
+
+fetch('./Cities.min.json')
+  .then(response => {
+    if (!response.ok) throw new Error('ファイル読み込み失敗');
+    return response.json();
+  })
+  .then(cities => {
+    console.log('読み込んだ都市一覧:', cities);
+    cities.forEach(city => {
+      const option = document.createElement('option');
+      option.value = city.code;
+      option.textContent = city.name;
+      Cities.appendChild(option);
+    });
+  })
+  .catch(error => {
+    console.error('エラー:', error);
+  });
